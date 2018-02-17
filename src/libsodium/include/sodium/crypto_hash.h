3972e3fc3fd1e666c8f7965a1b1dab70ace9c6ed
Steve Zeidner <szeidner@gmail.com>
2018-02-16 22:33:18 -0500

    Use old signing method.

--- src/libsodium/include/sodium/crypto_hash.h
+++ src/libsodium/include/sodium/crypto_hash.h
@@ -10,17 +10,17 @@
 
 #include <stddef.h>
 
-#include "crypto_hash_sha512.h"
+#include "crypto_generichash.h"
 #include "export.h"
 
 #ifdef __cplusplus
-# ifdef __GNUC__
-#  pragma GCC diagnostic ignored "-Wlong-long"
-# endif
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wlong-long"
+#endif
 extern "C" {
 #endif
 
-#define crypto_hash_BYTES crypto_hash_sha512_BYTES
+#define crypto_hash_BYTES crypto_generichash_BYTES
 SODIUM_EXPORT
 size_t crypto_hash_bytes(void);
 
@@ -28,10 +28,10 @@ SODIUM_EXPORT
 int crypto_hash(unsigned char *out, const unsigned char *in,
                 unsigned long long inlen);
 
-#define crypto_hash_PRIMITIVE "sha512"
+#define crypto_hash_PRIMITIVE "blake2b"
 SODIUM_EXPORT
 const char *crypto_hash_primitive(void)
-            __attribute__ ((warn_unused_result));
+    __attribute__((warn_unused_result));
 
 #ifdef __cplusplus
 }
