1526671a5854900490aaeec38a1e12befe84266c
Steve Zeidner <szeidner@gmail.com>
2018-03-05 17:26:33 -0500

    Remove crypto_align from other blake2b methods.

--- src/libsodium/crypto_generichash/blake2b/ref/blake2b-ref.c
+++ src/libsodium/crypto_generichash/blake2b/ref/blake2b-ref.c
@@ -30,8 +30,7 @@ static blake2b_compress_fn blake2b_compress = blake2b_compress_ref;
 static const uint64_t blake2b_IV[8] = {
     0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL, 0x3c6ef372fe94f82bULL,
     0xa54ff53a5f1d36f1ULL, 0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
-    0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
-};
+    0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL};
 
 /* LCOV_EXCL_START */
 static inline int
@@ -51,7 +50,8 @@ blake2b_is_lastblock(const blake2b_state *S)
 static inline int
 blake2b_set_lastblock(blake2b_state *S)
 {
-    if (S->last_node) {
+    if (S->last_node)
+    {
         blake2b_set_lastnode(S);
     }
     S->f[0] = -1;
@@ -62,7 +62,7 @@ static inline int
 blake2b_increment_counter(blake2b_state *S, const uint64_t inc)
 {
 #ifdef HAVE_TI_MODE
-    uint128_t t = ((uint128_t) S->t[1] << 64) | S->t[0];
+    uint128_t t = ((uint128_t)S->t[1] << 64) | S->t[0];
     t += inc;
     S->t[0] = (uint64_t)(t >> 0);
     S->t[1] = (uint64_t)(t >> 64);
@@ -83,7 +83,7 @@ blake2b_param_set_salt(blake2b_param *P, const uint8_t salt[BLAKE2B_SALTBYTES])
 
 static inline int
 blake2b_param_set_personal(blake2b_param *P,
-                           const uint8_t  personal[BLAKE2B_PERSONALBYTES])
+                           const uint8_t personal[BLAKE2B_PERSONALBYTES])
 {
     memcpy(P->personal, personal, BLAKE2B_PERSONALBYTES);
     return 0;
@@ -94,47 +94,47 @@ blake2b_init0(blake2b_state *S)
 {
     int i;
 
-    for (i  = 0; i < 8; i++) {
+    for (i = 0; i < 8; i++)
+    {
         S->h[i] = blake2b_IV[i];
     }
-    memset(S->t, 0, offsetof(blake2b_state, last_node) + sizeof(S->last_node)
-           - offsetof(blake2b_state, t));
+    memset(S->t, 0, offsetof(blake2b_state, last_node) + sizeof(S->last_node) - offsetof(blake2b_state, t));
     return 0;
 }
 
 /* init xors IV with input parameter block */
-int
-blake2b_init_param(blake2b_state *S, const blake2b_param *P)
+int blake2b_init_param(blake2b_state *S, const blake2b_param *P)
 {
-    size_t         i;
+    size_t i;
     const uint8_t *p;
 
     COMPILER_ASSERT(sizeof *P == 64);
     blake2b_init0(S);
-    p = (const uint8_t *) (P);
+    p = (const uint8_t *)(P);
 
     /* IV XOR ParamBlock */
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < 8; i++)
+    {
         S->h[i] ^= LOAD64_LE(p + sizeof(S->h[i]) * i);
     }
     return 0;
 }
 
-int
-blake2b_init(blake2b_state *S, const uint8_t outlen)
+int blake2b_init(blake2b_state *S, const uint8_t outlen)
 {
     blake2b_param P[1];
 
-    if ((!outlen) || (outlen > BLAKE2B_OUTBYTES)) {
+    if ((!outlen) || (outlen > BLAKE2B_OUTBYTES))
+    {
         sodium_misuse();
     }
     P->digest_length = outlen;
-    P->key_length    = 0;
-    P->fanout        = 1;
-    P->depth         = 1;
+    P->key_length = 0;
+    P->fanout = 1;
+    P->depth = 1;
     STORE32_LE(P->leaf_length, 0);
     STORE64_LE(P->node_offset, 0);
-    P->node_depth   = 0;
+    P->node_depth = 0;
     P->inner_length = 0;
     memset(P->reserved, 0, sizeof(P->reserved));
     memset(P->salt, 0, sizeof(P->salt));
@@ -142,62 +142,70 @@ blake2b_init(blake2b_state *S, const uint8_t outlen)
     return blake2b_init_param(S, P);
 }
 
-int
-blake2b_init_salt_personal(blake2b_state *S, const uint8_t outlen,
-                           const void *salt, const void *personal)
+int blake2b_init_salt_personal(blake2b_state *S, const uint8_t outlen,
+                               const void *salt, const void *personal)
 {
     blake2b_param P[1];
 
-    if ((!outlen) || (outlen > BLAKE2B_OUTBYTES)) {
+    if ((!outlen) || (outlen > BLAKE2B_OUTBYTES))
+    {
         sodium_misuse();
     }
     P->digest_length = outlen;
-    P->key_length    = 0;
-    P->fanout        = 1;
-    P->depth         = 1;
+    P->key_length = 0;
+    P->fanout = 1;
+    P->depth = 1;
     STORE32_LE(P->leaf_length, 0);
     STORE64_LE(P->node_offset, 0);
-    P->node_depth   = 0;
+    P->node_depth = 0;
     P->inner_length = 0;
     memset(P->reserved, 0, sizeof(P->reserved));
-    if (salt != NULL) {
-        blake2b_param_set_salt(P, (const uint8_t *) salt);
-    } else {
+    if (salt != NULL)
+    {
+        blake2b_param_set_salt(P, (const uint8_t *)salt);
+    }
+    else
+    {
         memset(P->salt, 0, sizeof(P->salt));
     }
-    if (personal != NULL) {
-        blake2b_param_set_personal(P, (const uint8_t *) personal);
-    } else {
+    if (personal != NULL)
+    {
+        blake2b_param_set_personal(P, (const uint8_t *)personal);
+    }
+    else
+    {
         memset(P->personal, 0, sizeof(P->personal));
     }
     return blake2b_init_param(S, P);
 }
 
-int
-blake2b_init_key(blake2b_state *S, const uint8_t outlen, const void *key,
-                 const uint8_t keylen)
+int blake2b_init_key(blake2b_state *S, const uint8_t outlen, const void *key,
+                     const uint8_t keylen)
 {
     blake2b_param P[1];
 
-    if ((!outlen) || (outlen > BLAKE2B_OUTBYTES)) {
+    if ((!outlen) || (outlen > BLAKE2B_OUTBYTES))
+    {
         sodium_misuse();
     }
-    if (!key || !keylen || keylen > BLAKE2B_KEYBYTES) {
+    if (!key || !keylen || keylen > BLAKE2B_KEYBYTES)
+    {
         sodium_misuse();
     }
     P->digest_length = outlen;
-    P->key_length    = keylen;
-    P->fanout        = 1;
-    P->depth         = 1;
+    P->key_length = keylen;
+    P->fanout = 1;
+    P->depth = 1;
     STORE32_LE(P->leaf_length, 0);
     STORE64_LE(P->node_offset, 0);
-    P->node_depth   = 0;
+    P->node_depth = 0;
     P->inner_length = 0;
     memset(P->reserved, 0, sizeof(P->reserved));
     memset(P->salt, 0, sizeof(P->salt));
     memset(P->personal, 0, sizeof(P->personal));
 
-    if (blake2b_init_param(S, P) < 0) {
+    if (blake2b_init_param(S, P) < 0)
+    {
         sodium_misuse();
     }
     {
@@ -210,40 +218,48 @@ blake2b_init_key(blake2b_state *S, const uint8_t outlen, const void *key,
     return 0;
 }
 
-int
-blake2b_init_key_salt_personal(blake2b_state *S, const uint8_t outlen,
-                               const void *key, const uint8_t keylen,
-                               const void *salt, const void *personal)
+int blake2b_init_key_salt_personal(blake2b_state *S, const uint8_t outlen,
+                                   const void *key, const uint8_t keylen,
+                                   const void *salt, const void *personal)
 {
     blake2b_param P[1];
 
-    if ((!outlen) || (outlen > BLAKE2B_OUTBYTES)) {
+    if ((!outlen) || (outlen > BLAKE2B_OUTBYTES))
+    {
         sodium_misuse();
     }
-    if (!key || !keylen || keylen > BLAKE2B_KEYBYTES) {
+    if (!key || !keylen || keylen > BLAKE2B_KEYBYTES)
+    {
         sodium_misuse();
     }
     P->digest_length = outlen;
-    P->key_length    = keylen;
-    P->fanout        = 1;
-    P->depth         = 1;
+    P->key_length = keylen;
+    P->fanout = 1;
+    P->depth = 1;
     STORE32_LE(P->leaf_length, 0);
     STORE64_LE(P->node_offset, 0);
-    P->node_depth   = 0;
+    P->node_depth = 0;
     P->inner_length = 0;
     memset(P->reserved, 0, sizeof(P->reserved));
-    if (salt != NULL) {
-        blake2b_param_set_salt(P, (const uint8_t *) salt);
-    } else {
+    if (salt != NULL)
+    {
+        blake2b_param_set_salt(P, (const uint8_t *)salt);
+    }
+    else
+    {
         memset(P->salt, 0, sizeof(P->salt));
     }
-    if (personal != NULL) {
-        blake2b_param_set_personal(P, (const uint8_t *) personal);
-    } else {
+    if (personal != NULL)
+    {
+        blake2b_param_set_personal(P, (const uint8_t *)personal);
+    }
+    else
+    {
         memset(P->personal, 0, sizeof(P->personal));
     }
 
-    if (blake2b_init_param(S, P) < 0) {
+    if (blake2b_init_param(S, P) < 0)
+    {
         sodium_misuse();
     }
     {
@@ -257,14 +273,15 @@ blake2b_init_key_salt_personal(blake2b_state *S, const uint8_t outlen,
 }
 
 /* inlen now in bytes */
-int
-blake2b_update(blake2b_state *S, const uint8_t *in, uint64_t inlen)
+int blake2b_update(blake2b_state *S, const uint8_t *in, uint64_t inlen)
 {
-    while (inlen > 0) {
+    while (inlen > 0)
+    {
         size_t left = S->buflen;
         size_t fill = 2 * BLAKE2B_BLOCKBYTES - left;
 
-        if (inlen > fill) {
+        if (inlen > fill)
+        {
             memcpy(S->buf + left, in, fill); /* Fill buffer */
             S->buflen += fill;
             blake2b_increment_counter(S, BLAKE2B_BLOCKBYTES);
@@ -274,7 +291,8 @@ blake2b_update(blake2b_state *S, const uint8_t *in, uint64_t inlen)
             S->buflen -= BLAKE2B_BLOCKBYTES;
             in += fill;
             inlen -= fill;
-        } else /* inlen <= fill */
+        }
+        else /* inlen <= fill */
         {
             memcpy(S->buf + left, in, inlen);
             S->buflen += inlen; /* Be lazy, do not compress */
@@ -286,18 +304,20 @@ blake2b_update(blake2b_state *S, const uint8_t *in, uint64_t inlen)
     return 0;
 }
 
-int
-blake2b_final(blake2b_state *S, uint8_t *out, uint8_t outlen)
+int blake2b_final(blake2b_state *S, uint8_t *out, uint8_t outlen)
 {
     unsigned char buffer[BLAKE2B_OUTBYTES];
 
-    if (!outlen || outlen > BLAKE2B_OUTBYTES) {
+    if (!outlen || outlen > BLAKE2B_OUTBYTES)
+    {
         sodium_misuse();
     }
-    if (blake2b_is_lastblock(S)) {
+    if (blake2b_is_lastblock(S))
+    {
         return -1;
     }
-    if (S->buflen > BLAKE2B_BLOCKBYTES) {
+    if (S->buflen > BLAKE2B_BLOCKBYTES)
+    {
         blake2b_increment_counter(S, BLAKE2B_BLOCKBYTES);
         blake2b_compress(S, S->buf);
         S->buflen -= BLAKE2B_BLOCKBYTES;
@@ -329,102 +349,124 @@ blake2b_final(blake2b_state *S, uint8_t *out, uint8_t outlen)
 }
 
 /* inlen, at least, should be uint64_t. Others can be size_t. */
-int
-blake2b(uint8_t *out, const void *in, const void *key, const uint8_t outlen,
-        const uint64_t inlen, uint8_t keylen)
+int blake2b(uint8_t *out, const void *in, const void *key, const uint8_t outlen,
+            const uint64_t inlen, uint8_t keylen)
 {
-    CRYPTO_ALIGN(64) blake2b_state S[1];
+    //CRYPTO_ALIGN(64)
+    blake2b_state S[1];
 
     /* Verify parameters */
-    if (NULL == in && inlen > 0) {
+    if (NULL == in && inlen > 0)
+    {
         sodium_misuse();
     }
-    if (NULL == out) {
+    if (NULL == out)
+    {
         sodium_misuse();
     }
-    if (!outlen || outlen > BLAKE2B_OUTBYTES) {
+    if (!outlen || outlen > BLAKE2B_OUTBYTES)
+    {
         sodium_misuse();
     }
-    if (NULL == key && keylen > 0) {
+    if (NULL == key && keylen > 0)
+    {
         sodium_misuse();
     }
-    if (keylen > BLAKE2B_KEYBYTES) {
+    if (keylen > BLAKE2B_KEYBYTES)
+    {
         sodium_misuse();
     }
-    if (keylen > 0) {
-        if (blake2b_init_key(S, outlen, key, keylen) < 0) {
+    if (keylen > 0)
+    {
+        if (blake2b_init_key(S, outlen, key, keylen) < 0)
+        {
             sodium_misuse();
         }
-    } else {
-        if (blake2b_init(S, outlen) < 0) {
+    }
+    else
+    {
+        if (blake2b_init(S, outlen) < 0)
+        {
             sodium_misuse();
         }
     }
 
-    blake2b_update(S, (const uint8_t *) in, inlen);
+    blake2b_update(S, (const uint8_t *)in, inlen);
     blake2b_final(S, out, outlen);
     return 0;
 }
 
-int
-blake2b_salt_personal(uint8_t *out, const void *in, const void *key,
-                      const uint8_t outlen, const uint64_t inlen,
-                      uint8_t keylen, const void *salt, const void *personal)
+int blake2b_salt_personal(uint8_t *out, const void *in, const void *key,
+                          const uint8_t outlen, const uint64_t inlen,
+                          uint8_t keylen, const void *salt, const void *personal)
 {
-    CRYPTO_ALIGN(64) blake2b_state S[1];
+    //CRYPTO_ALIGN(64)
+    blake2b_state S[1];
 
     /* Verify parameters */
-    if (NULL == in && inlen > 0) {
+    if (NULL == in && inlen > 0)
+    {
         sodium_misuse();
     }
-    if (NULL == out) {
+    if (NULL == out)
+    {
         sodium_misuse();
     }
-    if (!outlen || outlen > BLAKE2B_OUTBYTES) {
+    if (!outlen || outlen > BLAKE2B_OUTBYTES)
+    {
         sodium_misuse();
     }
-    if (NULL == key && keylen > 0) {
+    if (NULL == key && keylen > 0)
+    {
         sodium_misuse();
     }
-    if (keylen > BLAKE2B_KEYBYTES) {
+    if (keylen > BLAKE2B_KEYBYTES)
+    {
         sodium_misuse();
     }
-    if (keylen > 0) {
+    if (keylen > 0)
+    {
         if (blake2b_init_key_salt_personal(S, outlen, key, keylen, salt,
-                                           personal) < 0) {
+                                           personal) < 0)
+        {
             sodium_misuse();
         }
-    } else {
-        if (blake2b_init_salt_personal(S, outlen, salt, personal) < 0) {
+    }
+    else
+    {
+        if (blake2b_init_salt_personal(S, outlen, salt, personal) < 0)
+        {
             sodium_misuse();
         }
     }
 
-    blake2b_update(S, (const uint8_t *) in, inlen);
+    blake2b_update(S, (const uint8_t *)in, inlen);
     blake2b_final(S, out, outlen);
     return 0;
 }
 
-int
-blake2b_pick_best_implementation(void)
+int blake2b_pick_best_implementation(void)
 {
 /* LCOV_EXCL_START */
 #if defined(HAVE_AVX2INTRIN_H) && defined(HAVE_TMMINTRIN_H) && \
     defined(HAVE_SMMINTRIN_H)
-    if (sodium_runtime_has_avx2()) {
+    if (sodium_runtime_has_avx2())
+    {
         blake2b_compress = blake2b_compress_avx2;
         return 0;
     }
 #endif
 #if defined(HAVE_EMMINTRIN_H) && defined(HAVE_TMMINTRIN_H) && \
     defined(HAVE_SMMINTRIN_H)
-    if (sodium_runtime_has_sse41()) {
+    if (sodium_runtime_has_sse41())
+    {
         blake2b_compress = blake2b_compress_sse41;
         return 0;
     }
 #endif
 #if defined(HAVE_EMMINTRIN_H) && defined(HAVE_TMMINTRIN_H)
-    if (sodium_runtime_has_ssse3()) {
+    if (sodium_runtime_has_ssse3())
+    {
         blake2b_compress = blake2b_compress_ssse3;
         return 0;
     }
